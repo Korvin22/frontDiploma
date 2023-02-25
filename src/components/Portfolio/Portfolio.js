a8d7ed1ca1962ebe702c5d9efca33ef2dcceebd3
Dmitrii Gudemenko <dmitry.gudemenko@yandex.ru>
2023-02-25 06:12:05 +0300

    refs in portfolio in other page

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -6,15 +6,15 @@ function Portfolio(props) {
       <p className="portfolio__description">Портфолио</p>
       <ul className="portfolio__links">
         <div className="portfolio__item">
-          <a className="portfolio__link" href={"https://github.com/Korvin22"}>Статичный сайт</a>
+          <a className="portfolio__link" href={"https://github.com/Korvin22/how-to-learn"} target="_blank">Статичный сайт</a>
           <img src={link} alt="Ссылка" />
         </div>
         <div className="portfolio__item">
-          <a className="portfolio__link" href={"https://github.com/Korvin22"}>Адаптивный сайт</a>
+          <a className="portfolio__link" href={"https://github.com/Korvin22/russian-travel"} target="_blank">Адаптивный сайт</a>
           <img src={link} alt="Ссылка" />
         </div>
         <div className="portfolio__item">
-          <a className="portfolio__link" href={"https://github.com/Korvin22"}>Одностраничное приложение</a>
+          <a className="portfolio__link" href={"https://github.com/Korvin22/react-mesto-auth"} target="_blank">Одностраничное приложение</a>
           <img src={link} alt="Ссылка" />
         </div>
       </ul>
